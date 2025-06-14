9d6d051cf36bf0234ebeb646cd9657c207146b05
Mensis <vladislavlugovij7@gmail.com>
2025-06-14 23:19:19 +0300

    Git ignore modified

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -9,7 +9,7 @@ export const stripeWebhooks = async (request, response) => {
     let event;
 
     try {
-        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.CLERK_WEBHOOK_SECRET);
+        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (error) {
         response.status(400).send(`Webhook error: ${error.message}`);
     }
