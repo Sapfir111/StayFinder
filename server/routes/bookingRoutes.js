c95c8a0c0f0153b3a67d44998bde0a60585f61f0
Mensis <vladislavlugovij7@gmail.com>
2025-06-14 23:14:36 +0300

    Git ignore modified

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -13,7 +13,7 @@ const bookingRouter = express.Router();
 bookingRouter.post('/check-availability', checkAvailabilityAPI);
 bookingRouter.post('/book', protect, createBooking);
 bookingRouter.get('/user', protect, getUserBookings);
-bookingRouter.get('/hotels', protect, getHotelBookings);
+bookingRouter.get('/hotel', protect, getHotelBookings);
 
 bookingRouter.post('/stripe-payment', protect, stripePayment);
 
