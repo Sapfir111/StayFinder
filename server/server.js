e17c35f3557740c7acf122b1fe3a38af439d2783
Mensis <vladislavlugovij7@gmail.com>
2025-06-14 15:59:13 +0300

    clerk secret change

--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
-
 import connectDB from './configs/db.js';
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 connectDB();
+connectCloudinary();
 
 const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
@@ -19,6 +24,10 @@ app.use(clerkMiddleware());
 app.use('/api/clerk', clerkWebhooks);
 
 app.get('/', (req, res) => res.send('API is working fine'));
+app.use('/api/user', userRouter);
+app.use('/api/hotels', hotelRouter);
+app.use('/api/rooms', roomRouter);
+app.use('/api/bookings', bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 
