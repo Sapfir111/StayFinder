9aac04b86ac08c7755e042219b2603c6986adb42
Mensis <vladislavlugovij7@gmail.com>
2025-06-14 19:45:54 +0300

    stripe added

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,6 +1,7 @@
 import Booking from "../models/Booking.js";
 import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
+import stripe from "stripe";
 
 // Function to Check Availability of Room
 const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
@@ -118,5 +119,50 @@ export const getHotelBookings = async (req, res) => {
         res.json( {success: true, dashboardData: {totalBookings, totalRevenue, bookings}} );
     } catch (error) {
         res.json( {success: false, message: 'Failed to fetch bookings' } );
+    }
+};
+
+export const stripePayment = async (req, res) => {
+    try {
+        const { bookingId } = req.body;
+
+        const booking = await Booking.findById(bookingId);
+        const roomData = await Room.findById(booking.room).populate('hotel');
+        const totalPrice = booking.totalPrice;
+        const { origin } = req.headers;
+
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+        const line_items = [
+            // я створював акаунт на США, але якщо буде якась
+            // помилка з оплатою, це може бути
+            // у неспівпадінні валют (currency: 'usd')
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: roomData.hotel.name,
+                    },
+                    unit_amount: totalPrice * 100
+                },
+                quantity: 1,
+            }
+        ];
+
+        // Create Checkout Session
+        const session = await stripeInstance.checkout.sessions.create({
+            line_items,
+            mode: 'payment',
+            success_url: `${origin}/loader/my-bookings`,
+            cancel_url: `${origin}/my-bookings`,
+            metadata: {
+                bookingId,
+            }
+        });
+
+        res.json({ success: true, url: session.url });
+
+    } catch (error) {
+        res.json({ success: false, message: 'Payment Failed' });
     }
 };
