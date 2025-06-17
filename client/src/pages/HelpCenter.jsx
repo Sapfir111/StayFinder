f53ff8c9a35c9ff9d6096f452104c9ad9b0f150c
Mensis <vladislavlugovij7@gmail.com>
2025-06-17 16:41:34 +0300

    Formatting

---
+++ client/src/pages/HelpCenter.jsx
@@ -0,0 +1,15 @@
+import Title from "../../components/Title.jsx";
+
+const HelpCenter = () => {
+    return (
+        <section className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
+            <Title
+                title="Центр допомоги"
+                subTitle="У нашому Центрі допомоги ви знайдете корисні статті, поради та відповіді на найпоширеніші запитання."
+                align="left"
+            />
+        </section>
+    );
+};
+
+export default HelpCenter;
